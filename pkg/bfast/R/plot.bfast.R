a93b2f78fdc0dea1c45996f8ac8bcf0c431e1c55
janvb (Dr. ) <Janverbesselt@gmail.com>
2009-10-21 05:25:14 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bfast@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bfast/R/plot.bfast.R
+++ pkg/bfast/R/plot.bfast.R
@@ -1,115 +1,109 @@
-plot.bfast <- function(x,realdata=TRUE,sim=NULL,type=c("data","final","components","acf","largest","all","extra"),title=TRUE, ...)
+plot.bfast <- function(x,type=c("components","all","data","seasonal","trend","noise"), sim=NULL, largest=FALSE, main, ...)
 {                                                                                                      
     type <- match.arg(type)
-    
-    # PLOT ORIGINAL DATA
-    if(type=="data")
-        plot(x$Yt,main="Yt")
+    realdata <- is.null(sim)
+    Trend.bp <- !x$nobp$Vt
+    if(type=="largest" & !Trend.bp)
+        stop("No trend breakpoints")
+    title <- !missing(main)
     niter <- length(x$output)
-    if(type=="final")
-        idx <- niter
-    else if(type=="all")
-        idx <- 1:niter
-    if(type=="components" | type=="all")
-    {
-        if(type=="components") # adjusted!
-            opar <- par(mfrow=c(2,1))
-        else if(type=="all")
-            opar <- par(mfrow=c(2,niter))
-        for(i in idx)
-        {
-            out <- x$output[[i]]
-            
-            # PLOT TREND COMPONENT
-            plot(out$Vt)
-            lines(out$Tt, col = 4)
-            Trend.bp <- !x$nobp$Vt
-            if (Trend.bp)
-            {
-                title(paste("iteration_",i,"_YES_struc_trend.pdf",sep=''))
-                lines(out$bp.Vt)
-                lines(out$ci.Vt)
-                legend("topright",paste("Time of BP(s)",paste(out$Vt.bp,collapse=",")),col=2)
-            }
-            else
-                title(paste("iteration_",i,"_NO_struc_trend.pdf",sep=''))
-            if (!realdata)
-            {
-                lines(sim$time.series[,'abrupt'], col=1, lty=2) # orginal data
-                legend("bottomleft",c("estimated","simulated"), lty=c(1,2),col=1)
-            }
-            
-            # PLOT SEASONAL COMPONENT
-            plot(out$Wt)
-            lines(out$St,col=2)
-            Seas.bp <- !x$nobp$Wt
-            if(Seas.bp)
-            {
-                title(paste("iteration_",i,"_YES_struc_season",sep=''))
-                lines(out$bp.Wt)
-                lines(out$ci.Wt)
-                legend("topright",paste("Time of BP(s)",paste(out$Wt.bp,collapse=",")),col=2)
-            }
-            else
-                title(paste("iteration_",i,"_NO_struc_season.pdf",sep=''))
-            if (!realdata)
-            {
-                lines(sim$time.series[,'seasonal'], col=1, lty=2) # orginal data
-                legend("bottomleft",c("first run seasonality","first run estimated","simulated"), lty=c(1,1,2),col=c(1,2,1))
-            }  
-        }
-        par(opar)
-    }
-                         
     out <- x$output[[niter]]
-
     Tt <- out$Tt
     St <- out$St
     noise <- out$Nt
 
-    if(type=="components" | type=="all" | type=="final")
+    if(type=="data")
+    # Plot original data
+    {
+        if(!title)
+            main <- "Yt"
+        plot(x$Yt,main=main,...)
+    }
+    else if(type=="components")
+    # Plot components from final iteration
     {
         ft  <- cbind(seasonal= out$St, trend=out$Tt,remainder=out$Nt)
         tsp(ft) <- tsp(x$Yt)
         ft <- list(time.series = ft)
-        if(title==TRUE) main <- paste('no. iterations to estimate stable breakpoints:',niter) else main=''
-        if (realdata)
-        {
-          seasonal(ft,out,sim=NULL,main=main) # plotting function based on STL structure. How can we use e.g. plot.seasonal ?
-        }
-        if (!realdata)
-        {
-        seasonal(ft,out,sim=sim,main=main)
-        }      
+        if(!title) 
+            main <- paste('no. iterations to estimate stable breakpoints:',niter) 
+        seasonal(ft,out,sim=sim,main=main) # plotting function based on STL structure. How can we use e.g. plot.seasonal ?
     }
-    
-    if(type=="acf" | type=="all")
-        acf(noise) # autocorrelation plot of noise component
-    
-    if(type=="largest" | type=="all")
+    else if(type=="noise")
+    # Plot noise component
     {
-        if (Trend.bp)
+        require(forecast)
+        if(!title)
+            main <- "Noise component"
+        tsdisplay(noise, main=main, ...)
+    }
+    else
+    {
+        if(type=="all")
         {
-            plot(out$Vt)
-            title(paste("iteration_",niter,"_YES_struc_trend_MostSignificant Change",sep=''))
-            lines(out$Tt, col = 4)
-            lines(out$bp.Vt)
-            lines(out$ci.Vt)
-            if (!realdata) 
-            {  
-               lines(sim$time.series[,'abrupt'], col=1, lty=2) # orginal data
-                legend("bottomleft",c("estimated","simulated"), lty=c(1,2),col=c(4,1))
-            }
-            legend("bottomright",c("Magnitude of most sign change"), lty=c(1),col=6)
-            lines(x$jump,col=6)
-            points(x$jump, pch=14, cex=1,col=6)
+            idx <- 1:niter
+            opar <- par(mfrow=c(2,niter))
         }
+        else
+            idx <- niter
+        for(i in idx)
+        {
+            out <- x$output[[i]]
+            
+            if(type != "seasonal")
+            {
+                # PLOT TREND COMPONENT
+                if(type=="trend" & !title)
+                    main <- "Trend component"
+                else if(!title)
+                    main <- paste("Iteration ",i,": Trend",sep='')
+                plot(out$Vt,main=main,ylab="Vt",...)
+                lines(out$Tt, col = 4)
+                if (Trend.bp)
+                {
+                    lines(out$bp.Vt)
+                    lines(out$ci.Vt)
+                    legend("topright",paste("Time of BP(s)",paste(out$Vt.bp,collapse=",")),col=2)
+                }
+                if (!realdata)
+                {
+                    lines(sim$time.series[,'abrupt'], col=1, lty=2) # original data
+                    legend("bottomleft",c("estimated","simulated"), lty=c(1,2),col=1)
+                }
+                if(largest)
+                {
+                    # Mark largest jump
+                    legend("bottomright",c("Magnitude of most sign change"), lty=c(1),col=6)
+                    lines(x$jump,col=6)
+                    points(x$jump, pch=14, cex=1,col=6)
+                }
+            }
+            
+            if(type != "trend")
+            {
+                # PLOT SEASONAL COMPONENT
+                if(type=="seasonal" & !title)
+                    main <- "Seasonal component"
+                else if(!title)
+                    main <- paste("Iteration ",i,": Seasonal",sep='')
+                plot(out$Wt,main=main,ylab="Wt",...)
+                lines(out$St,col=2)
+                Seas.bp <- !x$nobp$Wt
+                if(Seas.bp)
+                {
+                    lines(out$bp.Wt)
+                    lines(out$ci.Wt)
+                    legend("topright",paste("Time of BP(s)",paste(out$Wt.bp,collapse=",")),col=2)
+                }
+                if (!realdata)
+                {
+                    lines(sim$time.series[,'seasonal'], col=1, lty=2) # orginal data
+                    legend("bottomleft",c("first run seasonality","first run estimated","simulated"), lty=c(1,1,2),col=c(1,2,1))
+                }  
+            }
+        }
+        if(type=="all")
+            par(opar)
     }
-    if(type=="extra")
-    {    
-    plot(out$Vt)
-    lines(out$Tt, col = 2)
-    lines(out$bp.Vt)
-    lines(out$ci.Vt)
-    }
+                   
 }
