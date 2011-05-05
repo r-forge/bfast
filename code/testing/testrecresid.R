1176108866d05c84472c62b2f29e296acf210dbc
janvb (Dr. ) <Janverbesselt@gmail.com>
2011-05-05 07:35:45 +0000

    added a folder with tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bfast@96 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ code/testing/testrecresid.R
@@ -0,0 +1,37 @@
+## convenience function for time series pre-processing
+## to response plus regressors (linear time trend, season
+## dummies, and harmonic season by default with order 1)
+tspp <- function(y, order = 1) {
+
+  ## data with trend and season factor
+  rval <- data.frame(
+    response = y,
+    trend = 1:length(y),
+    season = factor(cycle(y))
+  )
+
+  ## set up harmonic trend matrix as well
+  freq <- frequency(y)
+  harmon <- outer(2 * pi * as.vector(time(y)), 1:order)
+  harmon <- cbind(apply(harmon, 2, cos), apply(harmon, 2, sin))
+  colnames(harmon) <- if(order == 1) {
+    c("cos", "sin")
+  } else {
+    c(paste("cos", 1:order, sep = ""), paste("sin", 1:order, sep = ""))
+  }
+  if((2 * order) == freq) harmon <- harmon[, -(2 * order)]
+  rval$harmon <- harmon
+
+  ## return everything
+  return(rval)
+}
+
+library(strucchange)
+
+x <- ts(read.csv("tshistory.csv")$x, start = c(2000, 4), frequency = 23)
+ x <- tspp(x, order = 3)
+ x <- x[nrow(x):1,]
+rr <-  recresid(response ~ trend + harmon, data = x)
+write.csv(rr,"outputtest.csv") 
+ ?recresid
+ 
