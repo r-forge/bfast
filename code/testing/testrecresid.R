d453fd6952b2eb1d967d2d62be98d8d1e20d9d96
janvb <janvb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-06 15:47:19 +0000

    ubuntu test
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bfast@105 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- code/testing/testrecresid.R
+++ code/testing/testrecresid.R
@@ -71,4 +71,4 @@ rr <-  data.frame(
   byhand      = recresid2(xx, yy),
   logkappa    =  logkappa(xx, yy)
 )
-write.csv(rr,"outputtest_macpro.csv", row.names = FALSE)
+write.csv(rr,"outputtest_ubuntu.csv", row.names = FALSE)
