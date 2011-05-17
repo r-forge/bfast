eda21597e4f30d69aee4d73dd1c5eb0cef0f3e95
janvb (Dr. ) <Janverbesselt@gmail.com>
2011-05-17 21:11:31 +0000

    update webpage
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bfast@118 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -13,21 +13,23 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <!DOCTYPE html
 	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
   <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title><?php echo $group_name; ?></title>
-	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+	<!--<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />-->
+	<link rel="stylesheet" href="css/style.css">
   </head>
 
 <body>
 
-<!-- R-Forge Logo -->
-<table border="0" width="100%" cellspacing="0" cellpadding="0">
-<tr><td>
-<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
-</table>
+<div id="container">
+	
+<div id="header">
+<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a>	
+</div>
+
 
 
 <!-- get project title  -->
@@ -75,7 +77,6 @@ northern part of the study area. Three abrupt changes are detected in the trend 
 
 
 
-<!-- <p> The temporary windows builds can be dowloaded <a href="http://bfast.r-forge.r-project.org/bfast_1.1.zip"><strong>here</strong></a>.</p> -->
 
 <p> <strong> Applications: </strong> Deforestation, forest health monitoring and phenological change detection within time series of spatio-temporal data sets (satellite images).
 </p>
@@ -84,6 +85,8 @@ northern part of the study area. Three abrupt changes are detected in the trend 
 
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+
+</div><!-- close container -->
 
 </body>
 </html>
