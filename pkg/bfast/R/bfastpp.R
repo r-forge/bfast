444e418b607155ba25bcd596f6fab414fa49b16c
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-21 12:34:53 +0000

    Extended bfastpp() slightly to be more in line with bfastmonitor() and to optionally remove both season _and_ trend first.
    Added documentation as this function is now fully exported.
    
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bfast@311 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bfast/R/bfastpp.R
+++ pkg/bfast/R/bfastpp.R
@@ -1,28 +1,34 @@
-#########################
-## Auxiliary functions ##
-#########################
-    
-bfastpp <- function(y, order = 3,
+bfastpp <- function(data, order = 3,
   lag = NULL, slag = NULL, na.action = na.omit,
-  stl = c("none", "trend", "seasonal"))
+  stl = c("none", "trend", "seasonal", "both"))
 {
+  ## data needs to be ts
+  if(!is.ts(data)) data <- as.ts(data)
+
   ## STL pre-processing to try to adjust for trend or season
   stl <- match.arg(stl)
   if(stl != "none") {
-    stl_adjust <- function(x) x - stats::stl(x, s.window = "periodic")$time.series[, stl]
-    if(NCOL(y) > 1L) {
-      for(i in 1:NCOL(y)) y[,i] <- stl_adjust(y[,i])
+    stl_adjust <- function(x) {
+      x_stl <- stats::stl(x, s.window = "periodic")$time.series
+      switch(stl,
+        "trend" = x - x_stl[, "trend"],
+        "seasonal" = x - x_stl[, "seasonal"],
+        "both" = x - x_stl[, "trend"] - x_stl[, "seasonal"])
+    }
+    if(NCOL(data) > 1L) {
+      for(i in 1:NCOL(data)) data[,i] <- stl_adjust(data[,i])
     } else {
-      y <- stl_adjust(y)
+      data <- stl_adjust(data)
     }
   }
 
   ## check for covariates
-  if(NCOL(y) > 1L) {
-    x <- coredata(y)[, -1L]
-    y <- y[, 1L]
+  if(NCOL(data) > 1L) {
+    x <- coredata(data)[, -1L]
+    y <- data[, 1L]
   } else {
     x <- NULL
+    y <- data
   }
 
   ## data with trend and season factor
@@ -35,6 +41,7 @@ bfastpp <- function(y, order = 3,
   
   ## set up harmonic trend matrix as well
   freq <- frequency(y)
+  order <- min(freq, order)
   harmon <- outer(2 * pi * as.vector(time(y)), 1:order)
   harmon <- cbind(apply(harmon, 2, cos), apply(harmon, 2, sin))
   colnames(harmon) <- if(order == 1) {
