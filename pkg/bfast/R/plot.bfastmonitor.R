19945d5f93417a28688b27738067884df4272f2e
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-29 19:55:35 +0000

    added na.rm=TRUE argument
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bfast@315 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bfast/R/plot.bfastmonitor.R
+++ pkg/bfast/R/plot.bfastmonitor.R
@@ -1,4 +1,4 @@
-plot.bfastmonitor <- function(x, main = TRUE, ylab = "Data", ...)
+plot.bfastmonitor <- function(x, na.rm = TRUE, main = TRUE, ylab = "Data", ...)
 {
   if(isTRUE(main)) main <- if(is.na(x$breakpoint)) {
     "No break detected"
@@ -8,6 +8,7 @@ plot.bfastmonitor <- function(x, main = TRUE, ylab = "Data", ...)
   }
 
   y <- if(is.null(dim(x$data))) x$data else x$data[,1L]
+  if(na.rm) y <- na.omit(as.zoo(y))
   plot(y, type = "n", main = main, ylab = ylab, ...)
   lines(window(y, end = x$history[2]), col = "black")
   lines(window(y, start = x$history[1], end = x$history[2]),
@@ -15,7 +16,7 @@ plot.bfastmonitor <- function(x, main = TRUE, ylab = "Data", ...)
   lines(window(y, start = x$monitor[1]), col = "red")
 
   test_pred <- predict(x$model, newdata = x$tspp)
-  test_pred <- as.ts(zoo(test_pred, x$tspp$time))
+  test_pred <- zoo(test_pred, x$tspp$time, frequency = frequency(y))
   lines(test_pred, col = "blue", lwd = 1.5)
 
   abline(v = x$monitor[1], lty = 2, col = "black", lwd = 1)
