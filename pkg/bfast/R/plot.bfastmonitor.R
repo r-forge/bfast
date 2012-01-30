9b913c163bb1f9187e73d7662fc3971c95a92d5a
janvb <janvb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-30 09:07:40 +0000

    code update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bfast@322 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bfast/R/plot.bfastmonitor.R
+++ pkg/bfast/R/plot.bfastmonitor.R
@@ -12,7 +12,7 @@ plot.bfastmonitor <- function(x, na.rm = TRUE, main = TRUE, ylab = "Data", ...)
   plot(y, type = "n", main = main, ylab = ylab, ...)
   lines(window(y, end = x$history[2]), col = "black")
   lines(window(y, start = x$history[1], end = x$history[2]),
-    col = "green", type = "p", pch = 19, cex = 1)
+    col = "darkgreen", type = "p", pch = 19, cex = 1)
   lines(window(y, start = x$monitor[1]), col = "red")
 
   test_pred <- predict(x$model, newdata = x$tspp)
@@ -23,10 +23,10 @@ plot.bfastmonitor <- function(x, na.rm = TRUE, main = TRUE, ylab = "Data", ...)
   abline(v = x$breakpoint, lty = 2, col = "red", lwd = 2)
     
   legend("bottomleft", bty = "n",
-    c("History", "Stable history", "Monitoring", "Fit based on stable history", "Time of detected break"),
-    lty = c(1, NA, 1, 1, 2),
-    col = c("black", "green", "red", "blue", "red"),
-    pch = c(NA, 19, NA, NA, NA)
+    c("Historical data", "New data", "Stable history", "Fit based on stable history", "Start of the Monitoring period", "Time of detected break"),
+    lty = c(1, 1, NA, 1, 2, 2),
+    col = c("black", "red", "darkgreen", "blue", "black", "red"),
+    pch = c(NA, NA, 19, NA, NA, NA)
   )
   invisible(x)
 }
