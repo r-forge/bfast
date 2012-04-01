081b72736004477937b521db8b24a89c57cc63e6
janvb <janvb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-01 12:03:47 +0000

    new code for bfast fast
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bfast@391 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- code/bfastfast/bfastfast_bfastpp.R
+++ code/bfastfast/bfastfast_bfastpp.R
@@ -2,40 +2,119 @@
 ## For a new light version of bfast 
 ## based on bfastpp
 
-## Jan Verbesselt
-
+## Author Jan Verbesselt
 require(bfast)
 
-## Super simple BFAST
+## simple BFAST
 ## without decomposition - just by fitting a seasonal trend model
-## this might be ok for general purposes
+## this might be ok for general purposes - and fast processing
+## A bfast version that is flexible and deals with NA's
+
+##########
+## Data ##
+##########
 
 NDVIb <- as.ts(zoo(som$NDVI.b, som$Time))
 plot(NDVIb)
 NDVIb
 ## add random NA's to the time series!!!
+i <- as.integer(runif(40,min=0,max=length(NDVIb)))      
+NDVIb[i] <- NA     # with VI specific noise
 which(is.na(NDVIb))
+plot(NDVIb)
 
+##############
+## Modeling ##
+##############
+
+## set-up
 d <- bfastpp(NDVIb, order=3)
+# d$response <- na.omit(NDVIb) ## to retain 'ts' properties
+
+## testing 
+plot(efp(response ~ trend + harmon, data = d, type = "OLS-MOSUM", h = 0.125))
+
+## dating
 bp <- breakpoints(response ~ trend + harmon, data = d)
 breakpoints(bp)
+plot(bp)
+confint(bp)
 
+## check segmentation
 d$seg <- breakfactor(bp)
-m <- lm(response ~ seg/trend, data = d)
-m1 <- lm(response ~ seg/(trend + harmon), data = d) # model with breakpoints
-
-## plot the results
-plot(response ~ time, data = d, type = "l", col = "gray")
-lines(fitted(m) ~ time, data = d, lwd = 2, col = 1, pch=19, cex=0.2) # without break
-lines(fitted(m1) ~ time, data = d, lwd = 2, col = 'darkgreen', pch=19, cex=0.5) # with break
-
-summary(m1)
-summary(m)
-
-anova(m,m1)
+levels(d$seg) <- seq_along(levels(d$seg))
+m0 <- lm(response ~ trend + harmon, data = d)
+m1 <- lm(response ~ seg/trend + harmon,   data = d)
+m2 <- lm(response ~ seg/(trend + harmon), data = d)
+anova(m1, m2) # trend and seasonal break model is better!
+anova(m0, m1, m2) # how can you automatically derive from the anova model which model fits the data best?
+summary(m2)  # the best model
 
 
+## visualise
+plot(response ~ time, data = d, type = "l", col = "gray", pch=19, cex=0.1) ## data gaps are not visible here
+plot(NDVIb, col = "gray") ## here data gaps are visible
+# lines(fitted(m0) ~ time, data = d, lwd = 2, col = 1, pch=19, cex=0.2, lty=3) # without break
+# lines(fitted(m1) ~ time, data = d, lwd = 2, col = 'darkgreen', pch=19, lty=2) # with break in the trend
+lines(fitted(m2) ~ time, data = d, lwd = 2, col = 'blue', pch=19, cex=0.5,lty=4) # with break in the trend
+## breakpoints and confint
+ci <- confint(bp)
+timing <- d$time[ci$confint]
+abline(v=timing[2], col='red')  # this is not correct
 
+##
+d2 <- d
+d2$harmon <- d2$harmon * 0
+lines(d2$time, predict(m2, d2), col = 4, lty = 2)
+arrows(timing[1], 0.2, timing[3], 0.2, col= 'red', code=0, cex=2,lwd=2) # confidence interval
+
+## to do
+## 1) derive the magnitude of the detected trend breaks
+## 2) check output of confint() for influence of NA's (is the time estimate correct?) and the plotting
+
+## output needed
+## 1) detect trend breaks and identify the biggest trend break (if multple trend breaks)
+## 2) derive the time and magnitude of the all trend breaks
+## 3) the confidence interval of the trend break
+
+coef(m2)[1] # intercept 1
+coef(m2)[2] # intercept 2
+
+coef(m2)[3] # slope 1
+coef(m2)[4] # slope 2
+
+## what is the correct time of the breakpoint?
+confint(bp)
+bp
+
+predict(m2,d2)[ci$confint[2]]
+predict(m2,d2)[ci$confint[2]+1]
+
+ti <-  # time of the breakpoint
+y1 <- coef(m2)[1]+(timing[2]*coef(m2)[3])
+y2 <- coef(m2)[2]+(timing[2]*coef(m2)[4])
+y2-y1
+
+## determine the magnitude of the breakpoints
+Vt.nrbp <- length(bp.Vt$breakpoints)
+co <- coef(fm1) # final fitted trend model
+Mag <- matrix(NA,Vt.nrbp,3)
+for (r in 1:Vt.nrbp) 
+{
+	if (r==1) 
+		y1 <- co[1]+co[r+Vt.nrbp+1]*ti[Vt.bp[r]]
+	else 
+		y1 <- co[1]+co[r]+co[r+Vt.nrbp+1]*ti[Vt.bp[r]]
+	y2 <- (co[1]+co[r+1])+co[r+Vt.nrbp+2]*ti[Vt.bp[r]+1]
+	Mag[r,1] <- y1
+	Mag[r,2] <- y2
+	Mag[r,3] <- y2-y1   
+}
+index <- which.max(abs(Mag[,3]))
+m.x <- rep(Vt.bp[index],2)
+m.y <- c(Mag[index,1],Mag[index,2]) #Magnitude position
+Magnitude <- Mag[index,3] # Magnitude of biggest change
+Time <- Vt.bp[index]
 
 ## how to extract time of the break
 ## magnitude of the change
