8045f08126f42347eb7fef670b2d46c1ef4989a4
janvb (Dr. ) <Janverbesselt@gmail.com>
2012-04-13 10:28:04 +0000

    website update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bfast@392 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -81,4 +81,18 @@ Simulated 16-day MODIS NDVI time series. The period from 2004 until mid-2010 (i.
 </div><!-- close container -->
 
 </body>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-30862199-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </html>
+
